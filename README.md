be81fe1b951c50cd451dd05b5aedc4d85e3a5ec5
Anshrivastava9 <ansh.kvds0911@gmail.com>
2021-09-28 01:33:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-epchwc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-epchwc)
---
+++ demo.js
@@ -0,0 +1,98 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Firstgrid from './Firstgrid';
+
+export default function ControlledAccordions() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div>
+      <Accordion
+        expanded={expanded === 'panel1'}
+        onChange={handleChange('panel1')}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+            Personal Information
+          </Typography>
+          <Typography sx={{ color: 'text.secondary' }}></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <Firstgrid />
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === 'panel2'}
+        onChange={handleChange('panel2')}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2bh-content"
+          id="panel2bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+            Educational Qualification
+          </Typography>
+          <Typography sx={{ color: 'text.secondary' }}></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <Firstgrid />
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === 'panel3'}
+        onChange={handleChange('panel3')}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3bh-content"
+          id="panel3bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>Skills</Typography>
+          <Typography sx={{ color: 'text.secondary' }}></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <Firstgrid />
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === 'panel4'}
+        onChange={handleChange('panel4')}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4bh-content"
+          id="panel4bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+            Personal data
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
+            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
